83e4dc03d1a3e399fe868438800c8b6c6e440ae0
sajimathew <smathew@alpha-sense.com>
2017-11-22 10:51:40 -0500

    second

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ var express = require("express")
 var logger = require("morgan")
 var path = require("path")
 var bodyParser = require("body-parser")
-
+var neo4j = require("neo4j-driver").v1
 
 var app = express();
 
+var driver = neo4j.driver("bolt://localhost",neo4j.auth.basic("neo4j","neo4j"))
 
+var session = driver.session();
 //views
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -17,7 +19,19 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/',function(req,res){
-  res.send("Fuck yeah!!")
+    session
+    .run("MATCH (n:Ingredient) RETURN n LIMIT 25")
+    .then(function(result){
+      var ingredients = []
+      result.records.forEach(function(record){
+        ingredients.push(record._fields[0].properties.name)
+        console.log(record._fields)
+      })
+      res.render("index",{ingredients:ingredients})
+    })
+    .catch(function(err){
+      console.log(err)
+    });
 })
 app.listen(3000);
 console.log('Server started with port 3000');
